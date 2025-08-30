1e94ed8a3dbfbad82c293d4349b7610cacd5b5ec
Abbas Ansari <iamabbas.self@gmail.com>
2025-08-30 12:22:31 +0530

    add HomeAppBar widget with title and cart icon

---
+++ lib/features/shop/screens/home/widgets/home_appbar.dart
@@ -0,0 +1,29 @@
+
+import 'package:azyro_ecommerce_app/common/widgets/appbar/appbar.dart';
+import 'package:azyro_ecommerce_app/common/widgets/products/cart/cart_menu_icon.dart';
+import 'package:azyro_ecommerce_app/utils/constants/colors.dart';
+import 'package:azyro_ecommerce_app/utils/constants/text_strings.dart';
+import 'package:flutter/material.dart';
+
+class HomeAppBar extends StatelessWidget {
+  const HomeAppBar({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return AzyroAppBar(
+      title: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(AppTexts.homeAppbarTitle, style: Theme.of(context).textTheme.labelMedium!.apply(color: AppColors.grey),),
+        Text(AppTexts.homeAppbarSubTitle, style: Theme.of(context).textTheme.headlineSmall!.apply(color: AppColors.white),)
+      ],
+    ),
+    actions: [
+      AzyroCartCounterIcon(onPressed: (){},iconColor: AppColors.white,)
+    ],
+    );
+  }
+}
+
