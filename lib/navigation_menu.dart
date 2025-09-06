681cc9cfe0ff046fb56aa45b48ccdb780e79595b
Abbas Ansari <iamabbas.self@gmail.com>
2025-09-06 15:49:41 +0530

    update Bottom Navigation to add Store Screen

--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -1,4 +1,5 @@
 import 'package:azyro_ecommerce_app/features/shop/screens/home/home.dart';
+import 'package:azyro_ecommerce_app/features/shop/screens/store/store.dart';
 import 'package:azyro_ecommerce_app/utils/constants/colors.dart';
 import 'package:azyro_ecommerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
@@ -45,9 +46,9 @@ class NavigationController extends GetxController {
   final Rx<int> selectedIndex = 0.obs;
 
   final screens = [
-    const HomeScreen(),
-    const Text("Store Screen"),
-    const Text("Wishlist Screen"),
-    const Text("Profile Screen"),
+     const HomeScreen(),
+     const StoreScreen(),
+     Container(color: Colors.red,),
+     Container(color: Colors.green,),
   ];
 }
