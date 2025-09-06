0196f7096412aedbc52cd21459821a134d53c196
Abbas Ansari <iamabbas.self@gmail.com>
2025-09-06 15:46:04 +0530

    create reusable CircularImage widget (used in BrandCard widget)

---
+++ lib/common/widgets/images/circular_image.dart
@@ -0,0 +1,46 @@
+import 'package:azyro_ecommerce_app/utils/constants/colors.dart';
+import 'package:azyro_ecommerce_app/utils/constants/sizes.dart';
+import 'package:azyro_ecommerce_app/utils/helpers/helper_functions.dart';
+import 'package:flutter/material.dart';
+
+class CircularImage extends StatelessWidget {
+  const CircularImage({
+    super.key,
+    this.fit = BoxFit.cover,
+    required this.image,
+    this.isNetworkImage = false,
+    this.overlayColor,
+    this.backgroundColor,
+    this.width = 56,
+    this.height = 56,
+    this.padding = SizeConstants.cardRadiusM,
+  });
+
+  final BoxFit? fit;
+  final String image;
+  final bool isNetworkImage;
+  final Color? overlayColor;
+  final Color? backgroundColor;
+  final double width, height, padding;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: width,
+      height: height,
+      padding: EdgeInsets.all(padding),
+      decoration: BoxDecoration(
+        color:
+            AzyroHelperFunctions.isDarkMode(context)
+                ? AppColors.black
+                : AppColors.white,
+        borderRadius: BorderRadius.circular(100),
+      ),
+      child: Image(
+        fit: fit,
+        image: isNetworkImage ? NetworkImage(image) : AssetImage(image),
+        color: overlayColor,
+      ),
+    );
+  }
+}
