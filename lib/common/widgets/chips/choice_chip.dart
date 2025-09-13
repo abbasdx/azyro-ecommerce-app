c65e8b74eb1a9e18d28bc737b8b4feb7971b361d
Abbas Ansari <iamabbas.self@gmail.com>
2025-09-13 23:51:35 +0530

    update AzyroChoiceChip

--- lib/common/widgets/chips/choice_chip.dart
+++ lib/common/widgets/chips/choice_chip.dart
@@ -1,22 +1,32 @@
 import 'package:azyro_ecommerce_app/common/widgets/custom_shapes/containers/circular_container.dart';
 import 'package:azyro_ecommerce_app/utils/constants/colors.dart';
+import 'package:azyro_ecommerce_app/utils/helpers/helper_functions.dart';
 import 'package:flutter/material.dart';
 
 class AzyroChoiceChip extends StatelessWidget {
-  const AzyroChoiceChip({super.key});
+  const AzyroChoiceChip({super.key, required this.text, required this.selected, this.onSelected});
+
+  final String text;
+  final bool selected;
+  final void Function(bool)? onSelected;
 
   @override
   Widget build(BuildContext context) {
-    return ChoiceChip(
-      label: const Text('Green'),
-      selected: true,
-      onSelected: (value) {},
-      labelStyle: TextStyle(color: true ? AppColors.white : null),
-      avatar: const CircularContainer(width: 50, height: 50, backgroundColor: Colors.green,),
-      shape: CircleBorder(),
-      labelPadding: EdgeInsets.all(0),
-      padding: EdgeInsets.all(0),
-      backgroundColor: Colors.green,
+    final isColor = AzyroHelperFunctions.getColor(text)!=null;
+    return Theme(
+      data: Theme.of(context).copyWith(canvasColor: Colors.transparent),
+      child: ChoiceChip(
+        label: isColor? const SizedBox(): Text(text),
+        selected: selected,
+        onSelected: onSelected,
+        labelStyle: TextStyle(color: true ? AppColors.grey : null),
+        avatar: isColor? CircularContainer(width: 50, height: 50, backgroundColor: AzyroHelperFunctions.getColor(text)!,) : null,
+        labelPadding: isColor ? const EdgeInsets.all(0) : null, // Make icon
+        padding: isColor ? const EdgeInsets.all(0) : null,
+        shape: isColor ? const CircleBorder() : null,
+        // ignore: prefer_null_aware_operators
+        backgroundColor: isColor? AzyroHelperFunctions.getColor(text)! : null,
+      ),
     );
   }
 }
