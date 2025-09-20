ec1026b58ce87f751f4401c65260f77fb8991362
Abbas Ansari <iamabbas.self@gmail.com>
2025-09-20 21:42:21 +0530

    update Checkout Screen

--- lib/features/shop/screens/checkout/checkout.dart
+++ lib/features/shop/screens/checkout/checkout.dart
@@ -1,10 +1,18 @@
 import 'package:azyro_ecommerce_app/common/widgets/appbar/appbar.dart';
 import 'package:azyro_ecommerce_app/common/widgets/custom_shapes/containers/rounded_container.dart';
+import 'package:azyro_ecommerce_app/common/widgets/login_signup/success_screen/success_screen.dart';
+import 'package:azyro_ecommerce_app/common/widgets/products/cart/coupon_widget.dart';
 import 'package:azyro_ecommerce_app/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:azyro_ecommerce_app/features/shop/screens/checkout/widgets/billing_address_section.dart';
+import 'package:azyro_ecommerce_app/features/shop/screens/checkout/widgets/billing_amount_section.dart';
+import 'package:azyro_ecommerce_app/features/shop/screens/checkout/widgets/billing_payment_section.dart';
+import 'package:azyro_ecommerce_app/navigation_menu.dart';
 import 'package:azyro_ecommerce_app/utils/constants/colors.dart';
+import 'package:azyro_ecommerce_app/utils/constants/image_strings.dart';
 import 'package:azyro_ecommerce_app/utils/constants/sizes.dart';
 import 'package:azyro_ecommerce_app/utils/helpers/exports.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CheckoutScreen extends StatelessWidget {
   const CheckoutScreen({super.key});
@@ -30,35 +38,54 @@ class CheckoutScreen extends StatelessWidget {
               SizedBox(height: SizeConstants.spaceBtwSections),
 
               // Coupon TextField
+              AzyroCouponCode(),
+              const SizedBox(height: SizeConstants.spaceBtwSections),
+
+              // Billing Section
               AzyroRoundedContainer(
                 showBorder: true,
-                backgroundColor: isDark ? AppColors.dark : AppColors.white,
-                padding: const EdgeInsets.only(
-                  top: SizeConstants.sm,
-                  bottom: SizeConstants.sm,
-                  right: SizeConstants.sm,
-                  left: SizeConstants.md,
-                ),
-                child: Row(
+                padding: EdgeInsets.all(SizeConstants.md),
+                backgroundColor: isDark ? AppColors.black : AppColors.white,
+                child: Column(
                   children: [
-                    Flexible(
-                      child: TextFormField(
-                        decoration: const InputDecoration(
-                          hintText: 'Have a Promo CodEnter here!',
-                          border: InputBorder.none,
-                          focusedBorder: InputBorder.none,
-                          enabledBorder: InputBorder.none,
-                          errorBorder: InputBorder.none,
-                          disabledBorder: InputBorder.none,
-                        ),
-                      ),
-                    ),
-                    ElevatedButton(onPressed: () {}, child: Text('Apply')),
+                    // Pricing
+                    BillingAmountSection(),
+                    const SizedBox(height: SizeConstants.spaceBtwItems),
+
+                    // Divider
+                    Divider(),
+                    const SizedBox(height: SizeConstants.spaceBtwItems),
+                    // Payment Method
+                    BillingPaymentSection(),
+                    const SizedBox(height: SizeConstants.spaceBtwItems),
+                    // Address
+                    BillingAddressSection(),
+                    const SizedBox(height: SizeConstants.spaceBtwItems),
                   ],
                 ),
               ),
             ],
           ),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.only(
+          left: SizeConstants.defaultSpace,
+          right: SizeConstants.defaultSpace,
+          bottom: SizeConstants.defaultSpace,
+          top: SizeConstants.defaultSpace / 2.5,
+        ),
+        child: ElevatedButton(
+          onPressed:
+              () => Get.to(
+                () => SuccessScreen(
+                  image: AppImages.paymentSuccessIcon,
+                  title: 'Payment Success!',
+                  subTitle: 'Your item will be shipped soon!',
+                  onPressed: () => Get.offAll(() => const NavigationMenu()),
+                ),
+              ),
+          child: Text('Checkout \$149'),
         ),
       ),
     );
