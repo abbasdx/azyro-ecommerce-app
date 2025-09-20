5034695bc371bdc10af2b80c2f13c95302f58f35
Abbas Ansari <iamabbas.self@gmail.com>
2025-09-20 21:43:13 +0530

    update CartScreen

--- lib/features/shop/screens/cart/cart.dart
+++ lib/features/shop/screens/cart/cart.dart
@@ -1,9 +1,9 @@
 import 'package:azyro_ecommerce_app/common/widgets/appbar/appbar.dart';
-import 'package:azyro_ecommerce_app/common/widgets/products/cart/add_remove_button.dart';
-import 'package:azyro_ecommerce_app/common/widgets/products/cart/cart_item.dart';
-import 'package:azyro_ecommerce_app/common/widgets/texts/product_price_text.dart';
+import 'package:azyro_ecommerce_app/features/shop/screens/cart/widgets/cart_items.dart';
+import 'package:azyro_ecommerce_app/features/shop/screens/checkout/checkout.dart'; 
 import 'package:azyro_ecommerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
 class CartScreen extends StatelessWidget {
   const CartScreen({super.key});
@@ -17,36 +17,14 @@ class CartScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal:  SizeConstants.defaultSpace),
-        child: ListView.separated(
-          shrinkWrap: true,
-          separatorBuilder:
-              (_, __) => const SizedBox(height: SizeConstants.spaceBtwSections),
-          itemCount: 12,
-          itemBuilder:
-              (_, index) => Column(
-                children: [
-                  CartItem(),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Row(
-                        children: [
-                          const SizedBox(width: 70),
-                          // Add Remove Buttons
-                          ProductQuantityWithAddRemoveButton(),
-                        ],
-                      ),
-                      ProductPriceText(price: '149'),
-                    ],
-                  ),
-                ],
-              ),
-        ),
+        child: CartItems(),
       ),
+      // Checkout Button
       bottomNavigationBar: Padding(
         padding: const EdgeInsets.only(left: SizeConstants.defaultSpace, right: SizeConstants.defaultSpace, bottom: SizeConstants.defaultSpace, top: SizeConstants.defaultSpace/2.5),
-        child: ElevatedButton(onPressed: () {}, child: Text('Checkout \$149')),
+        child: ElevatedButton(onPressed: ()=> Get.to(()=> CheckoutScreen()), child: Text('Checkout \$149')),
       ),
     );
   }
 }
+
