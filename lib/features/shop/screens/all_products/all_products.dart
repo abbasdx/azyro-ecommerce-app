853841cbd897a350b340f5a4419ae7b636d679d0
Abbas Ansari <iamabbas.self@gmail.com>
2025-09-23 20:42:37 +0530

    create AllProducts Screen

---
+++ lib/features/shop/screens/all_products/all_products.dart
@@ -0,0 +1,20 @@
+import 'package:azyro_ecommerce_app/common/widgets/appbar/appbar.dart';
+import 'package:azyro_ecommerce_app/common/widgets/products/sortable/sortable_products.dart';
+import 'package:azyro_ecommerce_app/utils/constants/sizes.dart';
+import 'package:flutter/material.dart';
+
+class AllProducts extends StatelessWidget {
+  const AllProducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: const AzyroAppBar(title: Text('Popular Products',), showBackArrow: true,),
+      body: SingleChildScrollView(
+        child: Padding(padding: const EdgeInsets.all(SizeConstants.defaultSpace),
+        child: SortableProducts(),),
+      ),
+    );
+  }
+}
+
