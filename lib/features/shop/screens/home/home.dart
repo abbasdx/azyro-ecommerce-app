55fa24713f0aa73b6b9429721761e838a6a1a56b
Abbas Ansari <iamabbas.self@gmail.com>
2025-09-23 20:45:40 +0530

    update HomeScreen to add AllProducts

--- lib/features/shop/screens/home/home.dart
+++ lib/features/shop/screens/home/home.dart
@@ -3,6 +3,7 @@ import 'package:azyro_ecommerce_app/common/widgets/custom_shapes/containers/sear
 import 'package:azyro_ecommerce_app/common/widgets/layouts/grid_layout.dart';
 import 'package:azyro_ecommerce_app/common/widgets/products/product_cards/product_card_vertical.dart';
 import 'package:azyro_ecommerce_app/common/widgets/texts/section_heading.dart';
+import 'package:azyro_ecommerce_app/features/shop/screens/all_products/all_products.dart';
 import 'package:azyro_ecommerce_app/features/shop/screens/home/widgets/home_appbar.dart';
 import 'package:azyro_ecommerce_app/features/shop/screens/home/widgets/home_categories.dart';
 import 'package:azyro_ecommerce_app/features/shop/screens/home/widgets/promo_slider.dart';
@@ -10,6 +11,7 @@ import 'package:azyro_ecommerce_app/utils/constants/colors.dart';
 import 'package:azyro_ecommerce_app/utils/constants/image_strings.dart';
 import 'package:azyro_ecommerce_app/utils/constants/sizes.dart';
 import 'package:flutter/material.dart';
+import 'package:get/route_manager.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -52,6 +54,7 @@ class HomeScreen extends StatelessWidget {
                       ],
                     ),
                   ),
+                  const SizedBox(height: SizeConstants.spaceBtwSections,)
                 ],
               ),
             ),
@@ -75,7 +78,7 @@ class HomeScreen extends StatelessWidget {
                   const SizedBox(height: SizeConstants.spaceBtwSections,),
 
                   // Heading
-                  SectionHeading(title: 'Popular Products', onPressed: (){}),
+                  SectionHeading(title: 'Popular Products', onPressed: ()=> Get.to(()=> const AllProducts())),
 
                   // Popular Products
                   AzyroGridLayout(itemCount: 6, itemBuilder: (_, index ) => ProductCardVertical()), // GridView.builder
